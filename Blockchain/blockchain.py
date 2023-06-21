8684fb9f28618ba542478220674deb059de5669c
Alex Unnippillil <alex.unnippillil@hotmail.com>
2023-06-21 03:40:20 -0400

    Update blockchain.py

--- Blockchain/blockchain.py
+++ Blockchain/blockchain.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-Thereese are the requirementes of block:
+these are the requirementes of block:
     Hash Previous Block
     Timestmapnone
     MErkle Root HashHash(current Block)
